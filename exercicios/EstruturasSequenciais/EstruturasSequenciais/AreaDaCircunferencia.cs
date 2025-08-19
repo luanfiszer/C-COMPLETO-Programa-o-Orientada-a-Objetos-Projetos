b1d1e408675fe45fcf79d8f19565b0a6c770dab1
luanfiszer <luanbfiszer@gmail.com>
2025-08-18 21:57:49 -0300

    sequential structures exercise

---
+++ exercicios/EstruturasSequenciais/EstruturasSequenciais/AreaDaCircunferencia.cs
@@ -0,0 +1,13 @@
+﻿namespace EstruturasSequenciais;
+
+public class AreaDaCircunferencia
+{
+    static void areaDaCircunferencia()
+    {
+        double raio = double.Parse(Console.ReadLine());
+
+        double area = 3.14159 * Math.Pow(raio, 2);
+
+        Console.WriteLine(area);
+    }
+}
---
+++ exercicios/EstruturasSequenciais/EstruturasSequenciais/ContasComTresValores.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+
+namespace EstruturasSequenciais;
+
+public class ContasComTresValores
+{
+    static void Main()
+    {
+        Console.WriteLine("IMPRIMA O VALOR DE A");
+        double a = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        Console.WriteLine("IMPRIMA O VALOR DE B");
+        double b = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        Console.WriteLine("IMPRIMA O VALOR DE C");
+        double c = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        Console.WriteLine("TRIANGULO: " + ((a * c)/2).ToString("F3"));
+        Console.WriteLine("CIRCULO: " + 3.14159 * Math.Pow(c, 2));
+        Console.WriteLine("TRAPÉZIO: " + ( (a+b) / (2/c)).ToString("F3") );
+        Console.WriteLine("QUADRADO: " + Math.Pow(b, 2).ToString("F3") );
+        Console.WriteLine($"RETANGULO: {(a * b):F3} ");
+
+
+
+    }
+}
