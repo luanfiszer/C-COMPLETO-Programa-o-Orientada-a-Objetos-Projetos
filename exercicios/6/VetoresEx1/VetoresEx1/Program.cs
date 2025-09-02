dbc21443d24e4e19c9510c18c227bf20212ac458
luanfiszer <luanbfiszer@gmail.com>
2025-09-02 16:16:45 -0300

    vector study

---
+++ exercicios/6/VetoresEx1/VetoresEx1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace VetoresEx1;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        double[] vet = new double[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine($"Imprima a {i + 1}° altura");
+            vet[i] = double.Parse(Console.ReadLine());
+        }
+
+        double sum = 0.0;
+        for (int i = 0; i < n; i++)
+        {
+            sum += vet[i];
+        }
+
+        double avg = sum / n;
+
+        Console.WriteLine($"AVERAGE HEIGHT: {avg:F2}");
+    }
+}
---
+++ exercicios/6/VetoresEx2/VetoresEx2/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace VetoresEx2;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Escreva a quantidade dos produtos: ");
+        int n = int.Parse(Console.ReadLine());
+
+        Produto[] produtos = new Produto[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine($"Escreva o nome do {i + 1}º produto: ");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine($"Escreva o preço do {i + 1}º produto: ");
+            double preco = double.Parse(Console.ReadLine());
+            produtos[i] = new Produto(nome, preco);
+        }
+
+        double soma = 0.0;
+
+        for (int i = 0; i < n; i++)
+        {
+            soma += produtos[i].Preco;
+        }
+
+        double avg = soma / n;
+
+        Console.WriteLine($"AVARANGE PRICE: {avg}");
+
+    }
+}
