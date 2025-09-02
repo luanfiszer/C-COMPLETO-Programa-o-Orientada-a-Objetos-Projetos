d07ad887d8017ee3cfe2ff4442681f3394245320
luanfiszer <luanbfiszer@gmail.com>
2025-09-02 16:17:11 -0300

    vector exercise

---
+++ exercicios/6/Pensionato/Pensionato/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Pensionato;
+
+class Program
+{
+    static void Main()
+    {
+        Quarto[] quarto = new Quarto[10];
+
+        Console.WriteLine("Quantos quartos serão alugados? ");
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write($"Aluguel #{i + 1}: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+
+            Console.Write("Quarto (0-9): ");
+            int numeroQuarto = int.Parse(Console.ReadLine());
+
+            quarto[numeroQuarto] = new Quarto(nome, email);
+            
+            Console.WriteLine();
+        }
+
+        for (int i = 0; i < 10 ;i++)
+        {
+            if (quarto[i] != null)
+            {
+                Console.WriteLine($"{i} : {quarto[i]}");
+            }
+        }
+
+
+    }
+}
---
+++ exercicios/6/Pensionato/Pensionato/Quarto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace Pensionato
+{
+     class Quarto
+    {
+        string Nome { get; set; }
+        string Email { get; set; }
+
+        public Quarto(string nome, string email)
+        {
+            Nome = nome;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return $"{Nome}, {Email}";
+        }
+    }
+}
