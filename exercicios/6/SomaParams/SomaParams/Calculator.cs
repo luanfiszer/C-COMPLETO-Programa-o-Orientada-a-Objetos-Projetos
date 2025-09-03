c6fc697f55e8590f04dba7540141d4a1d435d8cb
luanfiszer <luanbfiszer@gmail.com>
2025-09-02 23:15:15 -0300

    Params study

---
+++ exercicios/6/SomaParams/SomaParams/Calculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomaParams
+{
+    public class Calculator
+    {
+        public static double Sum(params int[] numbers)
+        {
+            int sum = 0;
+            for (int i = 0; i < numbers.Length ; i++)
+            {
+                sum += numbers[i];
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ exercicios/6/SomaParams/SomaParams/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace SomaParams;
+
+class Program
+{
+    static void Main()
+    {
+       int s1 = (int)Calculator.Sum(1, 2, 3);  // deixei o (int) para lembrar do casting
+
+        Console.WriteLine(s1);
+    }
+}
